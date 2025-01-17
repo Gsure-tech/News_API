8d3b48de0eeae6f3ee74b308ddb70b432e320e15
gsure-tech <Geeescode@>
2025-01-17 23:39:57 +0100

    Add url variable

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import requests
 
 api_key = "060531c83c86433ba1de4c75f99209e5"
-url ="https://newsapi.org/v2/everything?q=tesla&from=2024-12-17&sortBy=publishedAt&apiKey=060531c83c86433ba1de4c75f99209e5"
+url =("https://newsapi.org/v2/everything?q=tesla&from=2024-12-17&"
+      "sortBy=publishedAt&apiKey="
+      "060531c83c86433ba1de4c75f99209e5")
 request = requests.get(url)
-content = request.text
-print(content)
+content = request.json()
+for article in content["articles"]:
+    print(article["title"])
