17254f68023373065f2647fe2cbc9d35b610d0e5
gsure-tech <Geeescode@>
2025-01-18 11:25:27 +0100

    implement email sender to send data consumed from API

--- main.py
+++ main.py
@@ -2,13 +2,24 @@ import requests
 from send_email import send_email
 
 api_key = "060531c83c86433ba1de4c75f99209e5"
-url =("https://newsapi.org/v2/everything?q=tesla&from=2024-12-17&"
-      "sortBy=publishedAt&apiKey="
+url =("https://newsapi.org/v2/everything?q=tesla&from="
+      "2024-12-18&sortBy=publishedAt&apiKey="
       "060531c83c86433ba1de4c75f99209e5")
+
+# Male request
 request = requests.get(url)
+
+# Get a dictionary with data
 content = request.json()
+# print(content)
+
+# # Access the article titles and description
 for article in content["articles"]:
-    message = f"{article['title']} {article['description']}"
+    topic = article["title"]
+    desc = article["description"]
+    message = topic + " \n" + desc
     send_email(message)
-    # print(article["title"])
-    # print(article["description"])
+    # message = f"{article["title"]} {article["description"]}"
+    # send_email(message)
+#     print(article["title"])
+#     print(article["description"])
